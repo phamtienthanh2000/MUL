807501739b0554286bf20490ddd06964a418d0fd
Pham Tien Thanh <yen17032000@gmail.com>
2021-10-14 21:09:29 +0700

    first commit

---
+++ src/main/java/com/MUL/MulApplication.java
@@ -0,0 +1,15 @@
+package com.MUL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulApplication.class, args);
+		System.out.println("Main runs");
+	}
+
+
+}
---
+++ src/test/java/com/MUL/MulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MUL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
